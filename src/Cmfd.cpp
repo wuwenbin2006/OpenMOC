8555e0641751de34c95a1d652966f346d9969fa7
wuwenbin2006 <wuwenbin2006@gmail.com>
2018-09-20 18:18:05 -0400

    It works good for generateTracks() and create FSRs. Verified by using Plotter to check the FSRs.

--- src/Cmfd.cpp
+++ src/Cmfd.cpp
@@ -1571,7 +1571,8 @@ void Cmfd::initializeGroupMap() {
  */
 int Cmfd::findCmfdSurface(int cell, LocalCoords* coords) {
   Point* point = coords->getHighestLevel()->getPoint();
-  cell = getGlobalCMFDCell(cell);
+  if (_geometry->isDomainDecomposed())
+    cell = getGlobalCMFDCell(cell);
   return _lattice->getLatticeSurface(cell, point);
 }
 

fc67d6926f7f8ba9bfddc667a0573d39815a4035
wuwenbin2006 <wuwenbin2006@gmail.com>
2018-09-26 20:03:22 -0400

    It works good for defining both non-uniform CMFD meshes and non-uniform pin-cell lattice.

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -2981,9 +2981,6 @@ void Geometry::initializeCmfd() {
   _cmfd->setWidthY(max_y - min_y);
   _cmfd->setWidthZ(max_z - min_z);
 
-  /* Intialize CMFD Maps */
-  _cmfd->initializeCellMap();
-
   /* Initialize the CMFD lattice */
   Point offset;
   offset.setX(min_x + (max_x - min_x)/2.0);
@@ -2991,6 +2988,10 @@ void Geometry::initializeCmfd() {
   offset.setZ(min_z + (max_z - min_z)/2.0);
 
   _cmfd->initializeLattice(&offset);
+  
+  /* Intialize CMFD Maps */
+  _cmfd->initializeCellMap();
+  
   _cmfd->setGeometry(this);
 
 #ifdef MPIx

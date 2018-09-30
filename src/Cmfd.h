ff3d90cd216e6cfa8a73616eeaa4c4a7d705f415
wuwenbin2006 <wuwenbin2006@gmail.com>
2018-09-30 12:39:36 -0400

    add some variables for parallel CMFD

--- src/Cmfd.h
+++ src/Cmfd.h
@@ -299,6 +299,15 @@ private:
 
   /* The number of on-domain cells in the z-direction */
   int _local_num_z;
+  
+  std::vector<int> _accumulate_lmx;
+  std::vector<int> _accumulate_lmy;
+  std::vector<int> _accumulate_lmz;
+  
+  //for substitution of _local_num_x, _local_num_y, _local_num_y, checking the 
+  //code is completely and fully changed to non-uniform version. Cuz 
+  //_local_num_x, _local_num_y, _local_num_y is used too much in the code.
+  int _local_num_xn, _local_num_yn, _local_num_zn;
 
   /* Size of _tally_memory array */
   long _total_tally_size;
